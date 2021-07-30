38a3843a990e664dcf2aa65eebfd0b27b983ffcb
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2021-07-30 15:01:55 +0200

    Add project files.

---
+++ PowerConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace PowerConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        }
+    }
+}
